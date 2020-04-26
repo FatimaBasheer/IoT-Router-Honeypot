2f08597249dc3e2efc102bb542390d31532bbd6c
KavithaEV <kavithavarughese@gmail.com>
2020-04-26 16:31:32 +0530

    CSRF token for authenticating into cuckoo website got

--- Cuckoo/upload.py
+++ Cuckoo/upload.py
@@ -1,10 +1,19 @@
 import requests
 
-url = 'http://127.0.0.1:81/submit/'
 
-file = {'file' : open('test.docx' , 'rb')}
+url = 'http://127.0.0.1:81'
 
-response = requests.post(url, files = file )
-print(response.status_code)
-print(response.text)
+#Getting the CSRF token for authentication
+response = requests.get(url)
+cookies_list = response.headers['Set-Cookie'].split(';')
+print(cookies_list)
+csrf_token = cookies_list[0].split('=')[1]
+print(csrf_token)
+
+
+
+# mal_file = {'file' : open('test.docx' , 'rb')}
+
+# print(response.status_code)
+# print(response.text)
 
