27992e9029ea779375817392b80f7e0d4dfab779
KavithaEV <kavithavarughese@gmail.com>
2020-04-26 17:05:42 +0530

    Still non working code for uploading file...

--- Cuckoo/upload.py
+++ Cuckoo/upload.py
@@ -3,17 +3,24 @@ import requests
 
 url = 'http://127.0.0.1:81'
 
-#Getting the CSRF token for authentication
+#Getting the CSRF token for authen
 response = requests.get(url)
 cookies_list = response.headers['Set-Cookie'].split(';')
 print(cookies_list)
-csrf_token = cookies_list[0].split('=')[1]
+csrf_token = cookies_list[0]
 print(csrf_token)
 
+#Forming the header for uploading the file
+cuckoo_headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36'
+}
+cuckoo_headers['Cookie'] = csrf_token
+print(cuckoo_headers)
+mal_file = {'file' : open('test.docx' , 'rb')}
 
-
-# mal_file = {'file' : open('test.docx' , 'rb')}
-
-# print(response.status_code)
-# print(response.text)
+#sending the file
+response = requests.post(url, headers= cuckoo_headers, files= mal_file)
+print(response.status_code)
+print(response.text)
 
